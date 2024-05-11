59f9fbd125903456417be41684d8cfa4da376d8c
Liu Jie <1765659336@qq.com>
2024-05-11 15:22:17 +0800

    feat: 添加jlg-ui演示demo

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import JlgFormUi from 'jlg-form-ui'
 import 'jlg-form-ui/dist/style.css'
+import JlgUI from 'jlg-ui';
+import 'jlg-ui/dist/style.css'
 
-createApp(App).use(JlgFormUi).mount('#app')
+
+createApp(App).use(JlgFormUi).use(JlgUI).mount('#app')
